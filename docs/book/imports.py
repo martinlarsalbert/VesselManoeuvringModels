7861ae4564581600922f696bbcd78de2b9d55dc3
martinlarsalbert <marale@kth.se>
2021-08-23 16:38:51 +0200

    wrote about manoueuvring simulation.

---
+++ docs/book/imports.py
@@ -0,0 +1,54 @@
+## Local packages:
+
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+%config Completer.use_jedi = False  ## (To fix autocomplete)
+
+## External packages:
+import pandas as pd
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+pd.set_option("display.max_columns", None)
+
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+if os.name == 'nt':
+    plt.style.use('book.mplstyle')  # Windows
+
+import plotly.express as px 
+import plotly.graph_objects as go
+
+import seaborn as sns
+import sympy as sp
+from sympy.physics.mechanics import (dynamicsymbols, ReferenceFrame,
+                                      Particle, Point)
+from sympy.physics.vector.printing import vpprint, vlatex
+from IPython.display import display, Math, Latex, Markdown
+from src.substitute_dynamic_symbols import run, lambdify
+
+import pyro
+
+import sklearn
+import pykalman
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+import statsmodels.api as sm
+
+from scipy.integrate import solve_ivp
+
+## Local packages:
+from src.data import mdl
+
+from src.symbols import *
+from src.parameters import *
+import src.symbols as symbols
+from src import prime_system
+from src.models import regression
+from src.visualization.regression import show_pred
+from src.visualization.plot import track_plot
+from src.equation import Equation
+
+## Load models:
+# (Uncomment these for faster loading):
+import src.models.vmm_abkowitz  as vmm_abkowitz 
