c8e7344d2ac9334e2291b70e4207e1eed43ae6ff
Martin Alexandersson <martin.alexandersson@sspa.se>
2022-10-07 10:42:40 +0200

    src.

--- vessel_manoeuvring_models/models/vmm_abkowitz_no_thrust.py
+++ vessel_manoeuvring_models/models/vmm_abkowitz_no_thrust.py
@@ -5,9 +5,9 @@ References:
 
 
 import sympy as sp
-from src.symbols import *
-from src.nonlinear_vmm_equations import *
-from src.models.vmm import Simulator, VMM
+from vessel_manoeuvring_models.symbols import *
+from vessel_manoeuvring_models.nonlinear_vmm_equations import *
+from vessel_manoeuvring_models.models.vmm import Simulator, VMM
 
 p = df_parameters["symbol"]
 
@@ -27,17 +27,17 @@ subs = [
 X_qs_eq = sp.Eq(
     X_D,
     p.Xu * u
-    + p.Xuu * u ** 2
-    + p.Xuuu * u ** 3
-    + p.Xvv * v ** 2
-    + p.Xrr * r ** 2
+    + p.Xuu * u**2
+    + p.Xuuu * u**3
+    + p.Xvv * v**2
+    + p.Xrr * r**2
     + +p.Xvr * v * r
-    + p.Xdeltadelta * delta ** 2
-    + p.Xudeltadelta * u * delta ** 2
+    + p.Xdeltadelta * delta**2
+    + p.Xudeltadelta * u * delta**2
     + p.Xvdelta * v * delta
     + p.Xuvdelta * u * v * delta
-    + p.Xuvv * u * v ** 2
-    + p.Xurr * u * r ** 2
+    + p.Xuvv * u * v**2
+    + p.Xurr * u * r**2
     + p.Xuvr * u * v * r
     + p.Xrdelta * r * delta
     + p.Xurdelta * u * r * delta
@@ -56,26 +56,26 @@ Y_qs_eq = sp.Eq(
     Y_D,
     p.Yv * v
     + p.Yr * r
-    + p.Yvvv * v ** 3
-    + p.Yvvr * v ** 2 * r
-    + p.Yrrr * r ** 3
-    + p.Yvrr * v * r ** 2
-    + p.Yuuv * u ** 2 * v
-    + p.Yuur * u ** 2 * r
+    + p.Yvvv * v**3
+    + p.Yvvr * v**2 * r
+    + p.Yrrr * r**3
+    + p.Yvrr * v * r**2
+    + p.Yuuv * u**2 * v
+    + p.Yuur * u**2 * r
     + p.Yuv * u * v
     + p.Yur * u * r
     + p.Ydelta * delta
-    + p.Ydeltadeltadelta * delta ** 3
+    + p.Ydeltadeltadelta * delta**3
     + p.Yudelta * u * delta
-    + p.Yuudelta * u ** 2 * delta
-    + p.Yvdeltadelta * v * delta ** 2
-    + p.Yvvdelta * v ** 2 * delta
-    + p.Yrdeltadelta * r * delta ** 2
-    + p.Yrrdelta * r ** 2 * delta
+    + p.Yuudelta * u**2 * delta
+    + p.Yvdeltadelta * v * delta**2
+    + p.Yvvdelta * v**2 * delta
+    + p.Yrdeltadelta * r * delta**2
+    + p.Yrrdelta * r**2 * delta
     + p.Yvrdelta * v * r * delta
     + p.Ythrustdelta * thrust * delta
     #   + p.Ythrust * thrust
-    + p.Y0 + p.Y0u * u + p.Y0uu * u ** 2,
+    + p.Y0 + p.Y0u * u + p.Y0uu * u**2,
 )
 
 fy_eq = fy_eq.subs(subs)
@@ -92,26 +92,26 @@ N_qs_eq = sp.Eq(
     N_D,
     p.Nv * v
     + p.Nr * r
-    + p.Nvvv * v ** 3
-    + p.Nvvr * v ** 2 * r
-    + p.Nrrr * r ** 3
-    + p.Nvrr * v * r ** 2
-    + p.Nuuv * u ** 2 * v
-    + p.Nuur * u ** 2 * r
+    + p.Nvvv * v**3
+    + p.Nvvr * v**2 * r
+    + p.Nrrr * r**3
+    + p.Nvrr * v * r**2
+    + p.Nuuv * u**2 * v
+    + p.Nuur * u**2 * r
     + p.Nuv * u * v
     + p.Nur * u * r
     + p.Ndelta * delta
-    + p.Ndeltadeltadelta * delta ** 3
+    + p.Ndeltadeltadelta * delta**3
     + p.Nudelta * u * delta
-    + p.Nuudelta * u ** 2 * delta
-    + p.Nrrdelta * r ** 2 * delta
+    + p.Nuudelta * u**2 * delta
+    + p.Nrrdelta * r**2 * delta
     + p.Nvrdelta * v * r * delta
-    + p.Nvdeltadelta * v * delta ** 2
-    + p.Nrdeltadelta * r * delta ** 2
-    + p.Nvvdelta * v ** 2 * delta
+    + p.Nvdeltadelta * v * delta**2
+    + p.Nrdeltadelta * r * delta**2
+    + p.Nvvdelta * v**2 * delta
     #    + p.Nthrustdelta * thrust * delta
     #    + p.Nthrust * thrust
-    + p.N0 + p.N0u * u + p.N0uu * u ** 2,
+    + p.N0 + p.N0u * u + p.N0uu * u**2,
 )
 
 mz_eq = mz_eq.subs(subs)
