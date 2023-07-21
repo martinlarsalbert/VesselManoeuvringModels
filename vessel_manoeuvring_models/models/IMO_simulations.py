674d441a4e271bb9477d420748b959ec78c4cdf8
Martin Alexandersson <martin.alexandersson@sspa.se>
2023-07-21 14:48:39 +0200

    Implemented the zigzag in the same way as the field study tests were conducted.

--- vessel_manoeuvring_models/models/IMO_simulations.py
+++ vessel_manoeuvring_models/models/IMO_simulations.py
@@ -7,8 +7,11 @@ def zigzag(
     model,
     u0: float,
     rev: float = None,
+    twa: float = None,
+    tws: float = None,
     angle: float = 10.0,
     heading_deviation: float = 10.0,
+    neutral_rudder_angle: float = 0.0,
     t_max: float = 1000.0,
     dt: float = 0.01,
     rudder_rate=2.32,
@@ -22,8 +25,18 @@ def zigzag(
     ----------
     u0 : float
         initial speed [m/s]
+    rev : float
+        propeller speed [1/s]
+    twa : float
+        true wind angle [rad]
+    tws : float
+        true wind speed [m/s]
     angle : float, optional
         Rudder angle [deg], by default 10.0 [deg]
+    heading_deviation : float
+        normally same as angle [deg] --> zigzag10/10 etc. Otherwise 10/5 or similar.
+    neutral_rudder_angle : float, by default 0.0
+        neutral rudder angle [deg] so that actual rudder angle delta_real = delta + neutral_rudder_angle
     t_max : float, optional
         max simulation time, by default 1000.0
     dt : float, optional
@@ -53,13 +66,14 @@ def zigzag(
 
     if not rev is None:
         df_["rev"] = rev
+    if not twa is None:
+        df_["twa"] = twa
+    if not tws is None:
+        df_["tws"] = tws
 
-    # y0 = dict(df_.iloc[0])
-    #
-    # for control_key in model.control_keys:
-    #    y0.pop(control_key)
-
-    zig_zag_angle = np.abs(heading_deviation)
+    zig_zag_angle = np.abs(heading_deviation) + np.abs(
+        neutral_rudder_angle
+    )  # Note nautral rudder angle here!
     direction = np.sign(angle)
 
     def course_deviated(t, states, control):
@@ -77,6 +91,13 @@ def zigzag(
     df_result = pd.DataFrame()
 
     ## 1)
+
+    t_local = np.arange(0, t_max, dt)
+    delta_ = direction * np.deg2rad(rudder_rate) * t_local
+    mask = np.abs(delta_) > np.deg2rad(np.abs(angle))
+    delta_[mask] = direction * np.abs(np.deg2rad(angle))
+    df_["delta"] = delta_ + np.deg2rad(neutral_rudder_angle)
+
     result = model.simulate(
         df_=df_,
         method=method,
@@ -97,12 +118,17 @@ def zigzag(
     df_ = pd.DataFrame(data=data, columns=df_result.columns, index=t_)
     if not rev is None:
         df_["rev"] = rev
+    if not twa is None:
+        df_["twa"] = twa
+    if not tws is None:
+        df_["tws"] = tws
 
-    t_local = t_ = np.arange(0, t_max, dt)
+    # t_local = np.arange(0, t_max, dt)
+    t_local = t_[t_ >= time] - time
     delta_ = np.deg2rad(angle) + direction * np.deg2rad(rudder_rate) * t_local
     mask = np.abs(delta_) > np.deg2rad(np.abs(angle))
     delta_[mask] = direction * np.abs(np.deg2rad(angle))
-    df_["delta"] = delta_
+    df_["delta"] = delta_ + np.deg2rad(neutral_rudder_angle)
 
     #
     result = model.simulate(
@@ -124,15 +150,22 @@ def zigzag(
     data = np.tile(df_result.iloc[-1], (len(t_), 1))
     df_ = pd.DataFrame(data=data, columns=df_result.columns, index=t_)
 
-    t_local = t_ = np.arange(0, t_max, dt)
+    # t_local = np.arange(0, t_max, dt)
+    t_local = t_[t_ >= time] - time
     delta_ = (
-        -direction * np.deg2rad(angle) + direction * np.deg2rad(rudder_rate) * t_local
+        # -direction * np.deg2rad(angle) + direction * np.deg2rad(rudder_rate) * t_local
+        -np.deg2rad(angle)
+        + direction * np.deg2rad(rudder_rate) * t_local
     )
     mask = np.abs(delta_) > np.deg2rad(np.abs(angle))
     delta_[mask] = direction * np.abs(np.deg2rad(angle))
-    df_["delta"] = delta_
+    df_["delta"] = delta_ + np.deg2rad(neutral_rudder_angle)
     if not rev is None:
         df_["rev"] = rev
+    if not twa is None:
+        df_["twa"] = twa
+    if not tws is None:
+        df_["tws"] = tws
 
     result = model.simulate(
         df_=df_,
@@ -145,5 +178,7 @@ def zigzag(
     result["delta"] = df_["delta"]
     df_result = pd.concat((df_result, result.iloc[1:]), axis=0)
     time = df_result.index[-1]
+
+    df_result["V"] = np.sqrt(df_result["u"] ** 2 + df_result["v"] ** 2)
 
     return df_result
