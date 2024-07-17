638ea224f25e5cd139457816331dc0f27f93d090
martinlarsalbert <marale@kth.se>
2024-07-17 15:26:41 +0200

    I'm a real programmer, I added some tests! :)

---
+++ tests/test_sigma_points.py
@@ -0,0 +1,55 @@
+import pytest
+from vessel_manoeuvring_models.sigma_points import sigma_points_UKF, sigma_points_CKF
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+
+@pytest.fixture
+def n():
+    n = np.random.randint(1,5)    
+    yield n
+
+@pytest.fixture
+def x(n):
+    x = np.random.random(size=(n,1))
+    yield x
+    
+@pytest.fixture
+def P(n):
+    P = np.random.random(size=(n,n))
+    yield P
+    
+def test_sigma_points_UKF_random(x,P):
+    
+    SP,W = sigma_points_UKF(x=x, P=P)
+    
+    
+def test_sigma_points_CKF_random(x,P):
+    
+    SP,W = sigma_points_CKF(x=x, P=P)
+    
+
+def test_sigma_points_UKF():
+    
+    x = np.array([
+    0.957166948242946,
+    0.485375648722841,   
+    ])
+    
+    P = np.array([
+        [0.818331408407438,	0.499770655255690],
+        [0.499770655255690,	0.858703285182333],
+    ])
+    
+    SP_true = np.array([
+        [0.957166948242946,	2.44280728441146,	1.45502939588428,	-0.528473387925569,	0.459304500601615],
+        [0.485375648722841,	0.983238096364171,	2.01123371511551,	-0.0124867989184890,	-1.04048241766983],
+    ])
+    
+    W_true = np.array([0.333333333333333,	0.166666666666667,	0.166666666666667,	0.166666666666667,	0.166666666666667])
+    
+    SP,W = sigma_points_UKF(x=x, P=P)
+
+    assert_almost_equal(actual=SP, desired=SP_true)
+    assert_almost_equal(actual=W, desired=W_true)
+    
---
+++ vessel_manoeuvring_models/sigma_points.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy.linalg import sqrtm
+from numpy import zeros, sqrt
+
+def sigma_points(x:np.ndarray, P:np.ndarray, kind:str='UKF'):
+    """Computes sigma points, either using unscented transform or using cubature.
+
+    Args:
+        x (np.ndarray): [n x 1] Prior mean
+        P (np.ndarray): [n x n] Prior covariance
+        kind (str, optional): 'UKF', or 'CKF'. Defaults to 'UKF'.
+
+    Returns:
+        SP          [n x 2n+1] UKF, [n x 2n] CKF. Matrix with sigma points
+        W           [1 x 2n+1] UKF, [1 x 2n] UKF. Vector with sigma point weights 
+    """
+   
+    if kind=='UKF':
+        return sigma_points_UKF(x=x, P=P)
+
+    elif kind=='CKF':
+        return sigma_points_CKF(x=x, P=P)
+    else:
+        ValueError('Incorrect kind of sigma point')
+
+def sigma_points_UKF(x:np.ndarray, P:np.ndarray,):
+    
+    x = x.flatten()
+    n = P.shape[0]
+    P_sqrt = sqrtm(P)
+    
+    W_0 = 1 - n/3 # x is Gaussian
+    N = 2*n+1
+
+    # Calculate sigma points:
+    W = zeros(N)
+    SP = zeros((n,N))
+
+    S = sqrt(n/(1-W_0))
+    W_n = (1-W_0)/(2*n)
+    W[0] = W_0
+    SP[:,0] = x.flatten()
+    
+    for i in range(0,n):
+        j = i + 1
+        W[j] = W_n
+        W[j+n] = W_n
+
+        sigma_point = x + S*P_sqrt[:,i]
+        SP[:,j] = sigma_point.flatten()
+        sigma_point = x -S*P_sqrt[:,i]
+        SP[:,j+n] = sigma_point.flatten()
+
+    return SP,W
+
+def sigma_points_CKF(x:np.ndarray, P:np.ndarray,):
+    
+    x = x.flatten()
+    
+    n = P.shape[0]
+    P_sqrt = sqrtm(P)
+    
+    N = 2*n
+            
+    # Calculate sigma points:
+    W = zeros(N)
+    SP = zeros((n,N))
+    
+    W_n = 1/(2*n)
+    S = sqrt(n)
+    for i in range(0,n):
+        W[i] = W_n
+        W[i+n] = W_n
+        
+        sigma_point = x + S*P_sqrt[:,i]
+        SP[:,i] = sigma_point.flatten()
+        sigma_point = x -S*P_sqrt[:,i]
+        SP[:,i+n] = sigma_point.flatten()
+        
+    return SP,W
